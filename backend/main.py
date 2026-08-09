2534cb1f4112f419722fa5e04173965ebf1d28f6
thomasjoseph18 <thomasjoseph18112006@gmail.com>
2026-08-09 21:23:51 +0530

    ok

--- backend/main.py
+++ backend/main.py
@@ -60,8 +60,14 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
+    allow_origins=[
+        "https://thomasjoseph18.github.io",
+        "https://farekeralam.onrender.com",
+        "http://localhost:3000",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
